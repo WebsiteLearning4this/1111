9e03e3446772d6a93eae8e060a288a44242e93fb
WebsiteLearning4this <127075884+WebsiteLearning4this@users.noreply.github.com>
2023-03-11 17:35:01 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,33 +1,93 @@
 const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-// Create a renderer and add it to the DOM
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(canvas.width, canvas.height);
+// Define game constants
+const BLOCK_SIZE = 10;
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.height;
 
-// Create a camera and add it to the scene
-const fov = 75;
-const aspect = canvas.width / canvas.height;
-const near = 0.1;
-const far = 1000;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 5;
+// Define game state variables
+let snake = [{ x: 50, y: 50 }];
+let direction = "right";
+let food = null;
 
-// Create a cube and add it to the scene
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-// Create a scene and add the camera and cube to it
-const scene = new THREE.Scene();
-scene.add(camera);
-scene.add(cube);
-
-// Render the scene
-function render() {
-  requestAnimationFrame(render);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+function drawSnake() {
+  ctx.fillStyle = "green";
+  snake.forEach(block => {
+    ctx.fillRect(block.x, block.y, BLOCK_SIZE, BLOCK_SIZE);
+  });
 }
-render();
+
+function updateSnake() {
+  // Move snake in current direction
+  let newHead = { x: snake[0].x, y: snake[0].y };
+  if (direction === "right") {
+    newHead.x += BLOCK_SIZE;
+  } else if (direction === "left") {
+    newHead.x -= BLOCK_SIZE;
+  } else if (direction === "up") {
+    newHead.y -= BLOCK_SIZE;
+  } else if (direction === "down") {
+    newHead.y += BLOCK_SIZE;
+  }
+
+  // Check for collision with walls or snake body
+  if (newHead.x < 0 || newHead.x >= CANVAS_WIDTH || newHead.y < 0 || newHead.y >= CANVAS_HEIGHT) {
+    gameOver();
+    return;
+  }
+  if (snake.some(block => block.x === newHead.x && block.y === newHead.y)) {
+    gameOver();
+    return;
+  }
+
+  // Add new head and remove tail
+  snake.unshift(newHead);
+  if (newHead.x === food.x && newHead.y === food.y) {
+    generateFood();
+  } else {
+    snake.pop();
+  }
+}
+
+function generateFood() {
+  // Generate new food block in random location
+  food = {
+    x: Math.floor(Math.random() * (CANVAS_WIDTH / BLOCK_SIZE)) * BLOCK_SIZE,
+    y: Math.floor(Math.random() * (CANVAS_HEIGHT / BLOCK_SIZE)) * BLOCK_SIZE
+  };
+
+  // Check that food is not on top of snake
+  if (snake.some(block => block.x === food.x && block.y === food.y)) {
+    generateFood();
+  }
+}
+
+function drawFood() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x, food.y, BLOCK_SIZE, BLOCK_SIZE);
+}
+
+function update() {
+  // Update game state
+  updateSnake();
+
+  // Clear canvas and draw game objects
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  drawSnake();
+  drawFood();
+
+  // Schedule next update
+  setTimeout(() => requestAnimationFrame(update), 100);
+}
+
+function gameOver() {
+  alert("Game over!");
+  location.reload();
+}
+
+function handleKeyPress(event) {
+  // Update direction based on arrow key press
+  if (event.code === "ArrowRight" && direction !== "left") {
+    direction = "right";
+  } else if (event.code === "ArrowLeft"
