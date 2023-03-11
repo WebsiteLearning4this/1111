f38f014d04e23d03888ab82b6c858a1466f8ba73
WebsiteLearning4this <127075884+WebsiteLearning4this@users.noreply.github.com>
2023-03-11 17:01:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-let displayValue = '';
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-function appendToDisplay(value) {
-  displayValue += value;
-  document.getElementById('result').value = displayValue;
+let x = 50;
+let y = 50;
+let speed = 5;
+
+function update() {
+  // Move player based on arrow keys
+  if (keys.ArrowLeft) {
+    x -= speed;
+  }
+  if (keys.ArrowRight) {
+    x += speed;
+  }
+  if (keys.ArrowUp) {
+    y -= speed;
+  }
+  if (keys.ArrowDown) {
+    y += speed;
+  }
+
+  // Draw player
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(x, y, 50, 50);
+
+  // Schedule next update
+  requestAnimationFrame(update);
 }
 
-function clearDisplay() {
-  displayValue = '';
-  document.getElementById('result').value = '';
-}
+// Track arrow key state
+const keys = {};
+window.addEventListener("keydown", event => {
+  keys[event.key] = true;
+});
+window.addEventListener("keyup", event => {
+  keys[event.key] = false;
+});
 
-function calculate() {
-  const result = eval(displayValue);
-  document.getElementById('result').value = result;
-  displayValue = '';
-}
+// Start game loop
+requestAnimationFrame(update);
